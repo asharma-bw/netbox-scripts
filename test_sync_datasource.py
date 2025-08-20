a86f4a87462214a4b21903d602ddda17f1f0a26e
Abhishek Sharma [C] abhisheksharma <abhisheksharma@U-3AY47ZUAFXD1T.bw-contractors.local>
2025-08-20 21:02:51 +0530

    copy sync script to main directory

---
+++ test_sync_datasource.py
@@ -0,0 +1,20 @@
+# from extras.jobs import Job
+from extras.scripts import Script
+from core.models import DataSource
+
+class SyncDataSourceJob(Script):
+    class Meta:
+        name = "Sync DataSource - Test"
+        description = "Test Manually sync a specific data source"
+        has_sensitive_variables = False
+
+    def run(self, data, commit):
+        self.log_info("Starting data source sync...")
+
+        # Get the data source by name or ID
+        try:
+            datasource = DataSource.objects.get(name="test-netbox-scripts")
+            datasource.sync() #Uncomment this to sync datasource.
+            self.log_success(f"Successfully synced data source: {datasource.name}")
+        except DataSource.DoesNotExist:
+            self.log_failure(f"Data source 'test-netbox-scripts' not found.")
